f80b260a87755c46cd150ebea6096a4281a39c18
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-08-22 20:47:27 -0400

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Gabriel J. P\xe9rez Irizarry'
+SITENAME = u'Dialy'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+THEME = 'startup-theme/'
+BOOTSTRAP_THEME = 'simplex'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+HIDE_SIDEBAR = True
+
+STATIC_PATHS = ['extra/custom.css']
+CUSTOM_CSS = 'static/custom.css'
+
+DEFAULT_PAGINATION = False
+
+BOOTSTRAP_FLUID = True
+
+# Product data
+
+SUB = u'messaging services'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
